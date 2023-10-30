5d3d1005b27c4201095fc007c1b78a7ca96c505f
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-10-31 07:59:10 +0800

    simple todo items

---
+++ client.js
@@ -0,0 +1,33 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const packageDef = protoLoader.loadSync('todo.proto', {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+
+const client = new todoPackage.Todo(
+  'localhost:40000',
+  grpc.credentials.createInsecure()
+);
+const text = process.argv[2];
+client.createTodo(
+  {
+    id: -1,
+    text: text,
+  },
+  (err, response) => {
+    if (err) {
+      console.log('Error:', err);
+      return;
+    }
+    console.log('Received from server', JSON.stringify(response));
+  }
+);
+
+client.readTodos({}, (err, response) => {
+  if (err) {
+    console.log('Error:', err);
+    return;
+  }
+  console.log('Received from server', JSON.stringify(response));
+  response.items.forEach((i) => console.log(i.id, i.text));
+});
---
+++ server.js
@@ -0,0 +1,35 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const packageDef = protoLoader.loadSync('todo.proto', {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+(async () => {
+  const server = new grpc.Server();
+
+  server.bindAsync(
+    '0.0.0.0:40000',
+    grpc.ServerCredentials.createInsecure(),
+    () => {
+      console.log('listening on 0.0.0.0:40000');
+      server.start();
+    }
+  );
+  server.addService(todoPackage.Todo.service, {
+    createTodo: createTodo,
+    readTodos: readTodos,
+  });
+
+  const todos = [];
+  function createTodo(call, callback) {
+    console.log(call);
+    const todoItem = {
+      id: todos.length + 1,
+      text: call.request.text,
+    };
+    todos.push(todoItem);
+    callback(null, todoItem);
+  }
+  function readTodos(call, callback) {
+    callback(null, { items: todos });
+  }
+})();
